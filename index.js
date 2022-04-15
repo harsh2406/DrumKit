d229f2f078f07416cae29518bd1da86910dc8f77
harsh2406 <55043223+harsh2406@users.noreply.github.com>
2022-04-15 16:46:34 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,65 @@
+// function for playing sound
+function makesound(key){
+    switch (key) {
+        case 'w':
+            var tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;
+        case 'a':
+            var tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+        case 's':
+            var tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play();
+            break;
+        case 'd':
+            var tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play();
+            break;
+        case 'j':
+            var snare = new Audio("sounds/snare.mp3");
+            snare.play();
+            break;
+        case 'k':
+            var crash = new Audio("sounds/crash.mp3");
+            crash.play();
+            break;
+        case 'l':
+            var kick = new Audio("sounds/flngekik.wav");
+            kick.play();
+            break;
+    
+        default: console.log(buttonHTML);
+            break;
+    }
+}
+
+// function for button animation
+function btnAnimation(currentKey){
+    var activeButton = document.querySelector("."+currentKey);
+    activeButton.classList.add("pressed");
+
+    setTimeout(function(){
+        activeButton.classList.remove("pressed");
+    }, 100);
+}
+
+// for click event
+var numOfDrumBtns = document.querySelectorAll(".drum").length;
+
+for(var i=0; i<numOfDrumBtns; i++){
+
+document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+    var buttonHTML = this.innerHTML;
+    makesound(buttonHTML);
+    btnAnimation(buttonHTML);
+});
+
+}
+
+// for keydown event
+document.addEventListener("keydown", function(event){
+    makesound(event.key);
+    btnAnimation(event.key);
+});
